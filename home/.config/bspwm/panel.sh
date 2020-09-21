06a8899a74c4f08615e69894cbca8256b86e2149
Sergey Shakur <sergey.shakur@gmail.com>
2020-09-21 09:37:26 +0300

    bspwm module added to polybar config, some keybindings added to bspwm config

--- home/.config/bspwm/panel.sh
+++ home/.config/bspwm/panel.sh
@@ -8,8 +8,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload bspwm-bar &
   done
 else
-  polybar --reload main &
+  polybar --reload bspwm-bar &
 fi

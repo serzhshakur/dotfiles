739d8684b33028acaa4d9fb8b1cd87c93292bb1c
Sergey Shakur <s.sakurs@bilderlings.com>
2021-05-16 16:07:55 +0300

    extracted bspwm rules to a separate script file

---
+++ home/.config/bspwm/rules.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+############ HELPERS ###########
+
+to_desktop() {
+  if [ "$#" -lt 2 ]; then
+    echo "wrong number of args. Usage: to_desktop desktop_label wm_classes"
+    return 1
+  fi
+
+  local DESKTOP=$1
+  shift
+
+  for wm_class in "$@"; do
+    bspc rule -a "$wm_class" desktop="$DESKTOP"
+  done
+}
+
+make_floating() {
+  if [ "$#" -lt 1 ]; then
+    echo "wrong number of args"
+    return 1
+  fi
+
+  for wm_class in "$@"; do
+    bspc rule -a "$wm_class" state=floating center=true
+  done
+}
+
+###############################
+
+to_desktop 'a' firefox
+to_desktop 'd' jetbrains-idea
+to_desktop 'o' KeePassXC
+to_desktop 'u' Slack Rambox Ferdi
+
+make_floating Lxappearance Nitrogen Blueman-manager Qalculate-gtk llpp Viewinor Nm-connection-editor pavucontrol jetbrains-toolbox System-config-printer.py
+
+# Getting rid of small annoying Slack window while having a call
+bspc rule -a "*:*:Slack | mini panel" hidden=on

51cb95fa1f3445289b031ea4c9d6bb7e88f2a1d4
Sergey Shakur <s.sakurs@bilderlings.com>
2021-06-28 09:57:39 +0300

    weather_lv.sh: show no error when something gone wrong while retrieving data

--- home/.config/polybar/scripts/weather_lv.sh
+++ home/.config/polybar/scripts/weather_lv.sh
@@ -28,6 +28,7 @@ declare -A icons=(
   [1309]=""
   [1310]=""
   [1311]=""
+  [1506]=""
 )
 
 get_time() {
@@ -39,51 +40,57 @@ get_time() {
 }
 
 get_value() {
-  local json=${1:?"argument 'source json' is missing"}
-  local key=${2:?"argument 'key' is missing"}
+  local json=$1
+  local key=$2
 
-  result=$(echo "$json" | jq -r --arg key "$key" '.[$key]')
-  echo "$result"
+  if [[ -n $json && -n $key ]]; then
+    result=$(echo "$json" | jq -r --arg key "$key" '.[$key]')
+    echo "$result"
+  fi
 }
 
 get_temp() {
-  local json=${1:?"argument 'source json' is missing"}
+  local json=$1
 
-  local curr=$(get_value "$response" "temperatura" | xargs printf "%.0f\n")
-  local icon_code=$(get_value "$response" "laika_apstaklu_ikona")
-  icon_code=${icon_code%.0} # removing trailing ".0"
+  if [[ -n $json ]]; then
+    local curr=$(get_value "$response" "temperatura" | xargs printf "%.0f\n")
+    local icon_code=$(get_value "$response" "laika_apstaklu_ikona")
 
-  local icon=${icons[$icon_code]}
+    icon_code=${icon_code%.0} # removing trailing ".0"
+    local icon=${icons[$icon_code]}
 
-  echo "+$curr°C $icon"
+    echo "+$curr°C $icon"
+  fi
 }
 
 get_wind() {
-  local json=${1:?"argument 'source json' is missing"}
+  local json=$1
 
-  local wind_dir=$(get_value "$response" "veja_virziens" | xargs printf "%.0f\n")
-  local wind_speed=$(get_value "$response" "veja_atrums" | xargs printf "%.0f\n")
+  if [[ -n $json ]]; then
+    local wind_dir=$(get_value "$response" "veja_virziens" | xargs printf "%.0f\n")
+    local wind_speed=$(get_value "$response" "veja_atrums" | xargs printf "%.0f\n")
 
-  dir_icon=""
-  if [[ $wind_dir -gt 340 || $wind_dir -lt 20 ]]; then
-    dir_icon=""
-  elif [[ $wind_dir -ge 20 && $wind_dir -lt 70 ]]; then
-    dir_icon=""
-  elif [[ $wind_dir -ge 70 && $wind_dir -lt 110 ]]; then
-    dir_icon=""
-  elif [[ $wind_dir -ge 110 && $wind_dir -lt 160 ]]; then
-    dir_icon=""
-  elif [[ $wind_dir -ge 160 && $wind_dir -lt 200 ]]; then
-    dir_icon=""
-  elif [[ $wind_dir -ge 200 && $wind_dir -lt 250 ]]; then
-    dir_icon=""
-  elif [[ $wind_dir -ge 250 && $wind_dir -lt 290 ]]; then
-    dir_icon=""
-  elif [[ $wind_dir -ge 290 && $wind_dir -lt 340 ]]; then
-    dir_icon=""
+    dir_icon=""
+    if [[ $wind_dir -gt 340 || $wind_dir -lt 20 ]]; then
+      dir_icon="↓"
+    elif [[ $wind_dir -ge 20 && $wind_dir -lt 70 ]]; then
+      dir_icon="↙"
+    elif [[ $wind_dir -ge 70 && $wind_dir -lt 110 ]]; then
+      dir_icon="←"
+    elif [[ $wind_dir -ge 110 && $wind_dir -lt 160 ]]; then
+      dir_icon="↖"
+    elif [[ $wind_dir -ge 160 && $wind_dir -lt 200 ]]; then
+      dir_icon="↑"
+    elif [[ $wind_dir -ge 200 && $wind_dir -lt 250 ]]; then
+      dir_icon="↗"
+    elif [[ $wind_dir -ge 250 && $wind_dir -lt 290 ]]; then
+      dir_icon="→"
+    elif [[ $wind_dir -ge 290 && $wind_dir -lt 340 ]]; then
+      dir_icon="↘"
+    fi
+
+    echo "$dir_icon$wind_speed m/s"
   fi
-
-  echo "$dir_icon$wind_speed m/s"
 }
 
 time=$(get_time)

fcfdf4d18965829aad28f6b78aef582f05bd9f20
Sergey Shakur <sergey.shakur@gmail.com>
2024-12-30 13:47:51 +0200

    prevent double bspwm config reload on boot

--- home/.config/autorandr/postswitch.d/1-bspwm.sh
+++ home/.config/autorandr/postswitch.d/1-bspwm.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-BSPWM_CFG_DIR=$HOME/.config/bspwm
+# bspwm will create a marker file on system boot. If autorandr finds the file, it
+# will remove it. This is to prevent double config reload on system boot.
+MARKER_FILE=/tmp/bspwm
 
-$BSPWM_CFG_DIR/multi-monitors.sh || true
-$BSPWM_CFG_DIR/panel.sh || true
-$BSPWM_CFG_DIR/reapply_rules.sh || true
+if [ -f $MARKER_FILE ]; then
+  rm $MARKER_FILE
+else
+  BSPWM_CFG_DIR=$HOME/.config/bspwm
 
-notify-send -i display "autorandr" "Switched profile to \"$AUTORANDR_CURRENT_PROFILE\""
+  $BSPWM_CFG_DIR/multi-monitors.sh || true
+  $BSPWM_CFG_DIR/panel.sh || true
+  $BSPWM_CFG_DIR/reapply_rules.sh || true
+
+  notify-send -i display "autorandr" "Switched profile to \"$AUTORANDR_CURRENT_PROFILE\""
+fi
